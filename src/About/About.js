ae146cbe780b4c3a83944d2037411d5d442f32a9
ShaliniTiwariiii <salinitiwari12345@gmail.com>
2023-01-11 17:44:24 +0530

    Routing part left

---
+++ src/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./About.css'
+import ReactPlayer from 'react-player'
+function About() {
+  return (
+    <div className='About'>
+      <ReactPlayer controls url='https://youtu.be/BR0jT6JxH-o'/>
+    </div>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+//import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Hero from './Components/Hero/Hero';
+import Program from './Components/Program/Program';
+import Reason from './Components/Reason/Reason';
+import Plans from './Components/Plans/Plans';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Join from './Components/Join/Join';
+import Footer from './Components/Footer/Footer';
+import About from './About/About';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+         <Hero/>
+         <Program/>
+         <Reason/>
+         <Plans/>
+         <Testimonials/>
+         <Join/>
+         <Footer/>
+  <About/>
+        
     </div>
   );
 }
